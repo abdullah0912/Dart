4381f46f4f1325f8c7ce28a3a794c7dcf817afcd
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-10-30 14:58:12 +0300

    Update tutorial_2.dart

--- A1/tutorial_2.dart
+++ A1/tutorial_2.dart
@@ -1,9 +1,19 @@
 void main() {
   var userName = 'Muhammed';
   var userMoney = 250;
+
   print(userName + ' ' + '$userMoney TL');
   /* var bir değişken tanımlama biçimidir.
      İçine değer atandıktan sonra veri tipi belli olur.
      yani değişken bir değişkendir :) Adı başında değeri değişen yani değişkendir.
+  */
+
+  final user_name = 'Hızır';
+  final user_money = 850;
+
+  print(user_name + ' ' + '$user_money TL');
+  /* var bir değişken tanımlama biçimidir.
+     Bu şekilde tanımlanan değişkenlere daha sonradan bir değer kesinlikle atılamaz.
+     Yani adından anlaşılabileceği gibi atanan değer final yani son değerdir.
   */
 }
