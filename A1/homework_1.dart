e690241c5adae0fc591cefb5e22f574a43878779
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-10-30 14:40:23 +0300

    Update homework_1.dart

--- A1/homework_1.dart
+++ A1/homework_1.dart
@@ -1,5 +1,17 @@
 void main() {
-  String user_name = 'Unal';
+  String user_name = 'Ünal';
   int user_money = 80;
-  user_money /= 2
+  user_money ~/= 20;
+
+  print(user_name + ' ' + 'bey paranız' + ' ' + '$user_money');
+  // int veri tipiyle yukarıdaki gibi tip dönüşümü için ~ işareti kullanılır
+
+  print('------------------');
+
+  // Eğer uygulamamızı double kullanarak yapsaydık aşağıdaki örnekteki gibi olacaktı
+  String userName = 'Ali';
+  double userMoney = 80;
+  userMoney /= 20;
+
+  print(userName + ' ' + 'bey paranız' + ' ' + '$userMoney');
 }
