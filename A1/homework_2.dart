a594cba19f131742667453a2fa8a7a0962c00522
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-11-02 09:06:53 +0300

    Update homework_2.dart

--- A1/homework_2.dart
+++ A1/homework_2.dart
@@ -1,6 +1,7 @@
 void main() {
   // Sabit bir banka ismi tanımla
-  // user1 ekle ve user1Money'i
+  // user1 ekle ve user1Money'e 100.000 değerini ver
+  // user2 ekle ve user2Money'e 500 değerini ver
   const bankName = 'Kasgar Bank';
   const user1Name = 'Ali';
   double user1Money = 100.000;
