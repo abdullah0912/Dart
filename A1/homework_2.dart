86358c39823134eb3408b684888468855c5f9cfe
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-11-02 09:03:03 +0300

    Update homework_2.dart

--- A1/homework_2.dart
+++ A1/homework_2.dart
@@ -1,3 +1,12 @@
 void main() {
-  
+  const bankName = 'Kasgar Bank';
+  const user1Name = 'Ali';
+  double user1Money = 100.000;
+
+  const String user2Name = 'Mehmet';
+  int user2Money = 500;
+
+  user2Money = user2Money - user1Money.toInt();
+
+  print('User 2 Money: $user2Money');
 }
