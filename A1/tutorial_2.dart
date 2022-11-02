732abb79702aa7abcc667b3e20e2b866012b0265
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2022-11-02 08:32:57 +0300

    dart_full_learn

--- A1/tutorial_2.dart
+++ A1/tutorial_2.dart
@@ -1,5 +1,5 @@
 void main() {
-  var userName = 'Muhammed';
+  var userName = 'Muhammed Ali';
   var userMoney = 250;
 
   print(userName + ' ' + '$userMoney TL');
@@ -8,12 +8,21 @@ void main() {
      yani değişken bir değişkendir :) Adı başında değeri değişen yani değişkendir.
   */
 
-  final user_name = 'Hızır';
-  final user_money = 850;
+  final String user_name = 'Hızır';
+  final int user_money = 850;
 
   print(user_name + ' ' + '$user_money TL');
   /* var bir değişken tanımlama biçimidir.
      Bu şekilde tanımlanan değişkenlere daha sonradan bir değer kesinlikle atılamaz.
      Yani adından anlaşılabileceği gibi atanan değer final yani son değerdir.
   */
+
+  final String userName2 = 'Engin';
+  final double userMoney2 = 85.350;
+
+  print(userName2 + ' ' + '${userMoney2 ~/ 2} TL');
+  // ~ işareti ile kalansız böler
+
+  print(userName2 + ' ' + '${userMoney2 / 2} TL');
+  // ~ işareti kullanılmadığı zaman ondalıklı biçimde böler
 }
